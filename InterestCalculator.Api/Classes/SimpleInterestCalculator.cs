7f46eab4d6f833035e58cda901dbfde0940f7331
Samir Banjanovic <sbanj17@gmail.com>
2019-08-12 10:58:34 -0400

    placed items in wrong project..moved to app not api

--- InterestCalculator.Api/Classes/SimpleInterestCalculator.cs
+++ InterestCalculator.Api/Classes/SimpleInterestCalculator.cs
@@ -3,13 +3,7 @@ namespace InterestCalculator.Api.Classes
     public class SimpleInterestCalculator
         : InterestCalculatorBase
     {
-        public SimpleInterestCalculator()
-        {
-        }
-
         public override decimal ComputeInterest(decimal balance, decimal interestRate)
-        {
-            throw new System.NotImplementedException();
-        }
+            => balance * interestRate;
     }
 }
