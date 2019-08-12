7bfba0d83919820b228827433e32a11bf0ea3e91
Samir Banjanovic <sbanj17@gmail.com>
2019-08-12 10:52:09 -0400

    decided to have app transmit interest rate instead of having a seperate component that pulls it

--- SamirBanjanovic.Clearent.CodeChallange.Tests/InterestCalculatorApiTests.cs
+++ SamirBanjanovic.Clearent.CodeChallange.Tests/InterestCalculatorApiTests.cs
@@ -1,7 +1,23 @@
+using InterestCalculator.Api.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace SamirBanjanovic.Clearent.CodeChallange.Tests
 {
+    [TestClass]
     public class InterestCalculatorApiTests
     {
-        
+        /*
+            Visa gets 10%
+            MC gets 5% interest
+            Discover gets 1% interest
+        */
+
+        [TestMethod]
+        public void TestSimpleInterestCalc()
+        {
+            IInterestCalculator interestCalc = new SimpleInterestCalculator();
+
+
+        } 
     }
 }
